456138bf199c4bf077c7fe34f32f5823450887bf
Rifky <akunkhusucsgo@gmail.com>
2025-09-11 15:13:04 +0700

    Initial commit with model and source code

---
+++ model/best_model_export/saved_model.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:107ecb0f3c5ad465e409e90234cd5cbef3c8dcc4a463629d3505ee3e1623fe26
+size 1751882
--- src/konversi_model.py
+++ src/konversi_model.py
@@ -1,25 +1,6 @@
-from tensorflow.keras.models import load_model
-import os
+from tensorflow import keras
 
-# Tentukan path ke folder model
-model_path = os.path.join('model', 'best_model.keras')
+model = keras.models.load_model("model/sugarcane_classifier_model.keras")
+model.save("model/best_model_v2.keras")
 
-# Cek apakah folder tersebut ada
-if not os.path.isdir(model_path):
-    print(f"Error: Folder tidak ditemukan di '{model_path}'")
-else:
-    try:
-        # Muat model dari format folder (SavedModel)
-        print(f"Mencoba memuat model dari folder '{model_path}'...")
-        model = load_model(model_path)
-        print("Model berhasil dimuat dari folder.")
-
-        # Simpan kembali model dalam format .h5 tunggal
-        output_path = 'model/converted_model.h5'
-        print(f"Menyimpan model ke '{output_path}'...")
-        model.save(output_path)
-
-        print(f"Model berhasil dikonversi dan disimpan sebagai '{output_path}'")
-
-    except Exception as e:
-        print(f"Terjadi error saat memuat atau mengonversi model: {e}")
+print("✅ Model berhasil disimpan ulang ke best_model_v2.keras")
